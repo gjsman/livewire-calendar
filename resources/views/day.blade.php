a56af0fe224cc0608ce7bdde07be738d8eafe98a
TamsinRose <108128203+TamsinRose@users.noreply.github.com>
2022-06-24 15:10:51 +0000

    Update day.blade.php

--- resources/views/day.blade.php
+++ resources/views/day.blade.php
@@ -16,7 +16,7 @@
             @if($dayClickEnabled)
                 wire:click="onDayClick({{ $day->year }}, {{ $day->month }}, {{ $day->day }})"
             @endif
-            class="w-full h-full p-2 @if($day->format('l')=='Saturday' || $day->format('l')'Sunday') {{ 'bg-gray-100' }} @endif {{ $dayInMonth ? $isToday ? 'bg-yellow-100' : ' bg-white ' : 'bg-gray-100' }} flex flex-col">
+            class="w-full h-full p-2 @if($day->format('l')=='Saturday' || $day->format('l')=='Sunday') {{ 'bg-gray-100' }} @endif {{ $dayInMonth ? $isToday ? 'bg-yellow-100' : ' bg-white ' : 'bg-gray-100' }} flex flex-col">
 
             {{-- Number of Day --}}
             <div class="flex items-center">
